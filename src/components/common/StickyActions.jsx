d9bb2f600e4eb7d7c4d619e252fcd89dbde5c539
avisinghss <anubhavsinghrr@gmail.com>
2026-07-24 00:42:11 +0530

    .

--- src/components/common/StickyActions.jsx
+++ src/components/common/StickyActions.jsx
@@ -13,22 +13,13 @@ export default function StickyActions() {
       {/* 1. Phone Call Action Button */}
       <a
         href={`tel:${phoneNumber}`}
-        className="flex items-center justify-center w-12 h-12 bg-[#b31919]/40 hover:bg-[#911414]/80 text-white shadow-lg transition-all duration-300 active:scale-95 rounded-r-[24px] border-y border-r border-white/10"
+        className="flex items-center justify-center w-12 h-12 bg-[#b31919]/40 hover:bg-[#911414]/80 text-white shadow-lg transition-all duration-300 active:scale-95 rounded-r-[24px] border-y border-r border-white/10 backdrop-blur-sm"
         aria-label="Call Us"
       >
         <Phone size={24}/>
       </a>
 
-      {/* 2. WhatsApp Direct Chat Button */}
-      {/* <a
-        href={`https://wa.me/${whatsAppNumber}?text=${whatsAppMessage}`}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="flex items-center justify-center w-14 h-14  bg-[#25D366] hover:bg-[#20ba5a] text-white shadow-lg transition-all duration-300 active:scale-95 rounded-r-[24px] border-y border-r border-white/10"
-        aria-label="Chat on WhatsApp"
-      >
-        <MessageCircleCode size={26} />
-      </a> */}
+      {/* 2. WhatsApp Chat Action Button */}
 <a
   href={`https://wa.me/${whatsAppNumber}?text=${encodeURIComponent(whatsAppMessage)}`}
   target="_blank"
